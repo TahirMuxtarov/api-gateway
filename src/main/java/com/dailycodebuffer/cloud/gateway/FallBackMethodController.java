60b3005c1498773d747771df5d316e50056a8cf5
Tahir <tahir-muxtarov@mail.ru>
2022-02-03 14:40:44 +0400

    filters added for fallback into the application.yml

--- src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
+++ src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FallBackMethodController {
 
-    @GetMapping("/userServiceFallBackMethod")
+    @GetMapping("/userServiceFallBack")
     public String userServiceFallBackMethod(){
         return "User Service is taking longar than expected" +
                 ", Please try again later";
     }
-    @GetMapping("/departmentServiceFallBackMethod")
+    @GetMapping("/departmentServiceFallBack")
     public String departmentServiceFallBackMethod(){
         return "Department Service is taking longar than expected" +
                 ", Please try again later";
